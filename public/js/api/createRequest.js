679cabae9e217277a35ea708548bfdf8217bc4a7
krash76 <krash76@mail.ru>
2021-12-14 13:26:24 +0100

    createRequest.js changed

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,29 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
 
+const createRequest = (method, url, data, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+
+  //  if (method === "GET") {
+  try {
+    xhr.open(method, url, [async, data.mail, data.password]);
+    xhr.send(data);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const answer = JSON.parse(xhr.responseText);
+      console.log(answer); //   здесь будет positive callback
+    }
+  } catch (e) {
+    // перехват сетевой ошибки
+    callback(e);
+    //}
+  }
 };
+
+createRequest(
+  "GET",
+  "http://localhost:5500/public/",
+  { mail: "ivan@biz.pro", password: "odinodin" },
+  () => console.log("error") //negative callback
+);
